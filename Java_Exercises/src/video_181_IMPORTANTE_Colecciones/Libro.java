badbdb4241cf8088138d57a332885a7d2d8a9cd1
firedevelop <firedevelop.com@gmail.com>
2017-09-26 18:11:20 +0200

    .
    .

---
+++ Java_Exercises/src/video_181_IMPORTANTE_Colecciones/Libro.java
@@ -0,0 +1,29 @@
+package video_181_IMPORTANTE_Colecciones;
+
+public class Libro{
+	public Libro(String titulo, String autor,int ISBN) {
+		this.titulo=titulo;
+		this.autor=autor;
+		this.ISBN=ISBN;
+	}
+	public String getDatos() {
+		return "El titulo es: " + titulo + ". El autor es: "+ autor + ". Y el ISBN es:" + ISBN;
+		
+	}
+	public boolean equals(Object obj) {
+		if(obj instanceof Libro) {
+			Libro otro=(Libro)obj;
+			if(this.ISBN==otro.ISBN) {
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			return false;
+		}
+	}
+	
+	private String titulo;
+	private String autor;
+	public int ISBN;
+}
