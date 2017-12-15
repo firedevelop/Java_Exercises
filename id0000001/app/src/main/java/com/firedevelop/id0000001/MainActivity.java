84a4e7e686e64f23d31acdcd876520095b6e64be
firedevelop <firedevelop.com@gmail.com>
2017-12-15 09:47:16 +0100

    .
    .

---
+++ id0000001/app/src/main/java/com/firedevelop/id0000001/MainActivity.java
@@ -0,0 +1,17 @@
+package com.firedevelop.id0000001;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void ayuda(View vista){
+        Intent intencion=
+    }
+}
